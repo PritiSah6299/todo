0d6f23aaf387d5f349bba8be8ca5f99fa35dd36c
PritiSah6299 <priti.sah@shell.com>
2024-02-09 17:18:17 +0530

    Modified the app using reduxToolkit

---
+++ src/StyledComponent.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledButton=styled.button`
+background-color: blue;
+color: white;
+padding 10px 20px;
+border: none;
+border-radius: 5px;
+cursor: pointer;
+`;
+
+export default StyledButton;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,13 @@
 // src/TodoList.js
 import React from 'react';
 import Todo from './Todo';
-import { useMemo } from 'react';
 
-const TodoList = ({ todos, onToggle }) => {
+const TodoList = ({ todos, dispatch }) => {
 
   return (
     <div style={{alignItems:'left',display: 'flex',flexDirection: 'column',justifyContent: 'center',marginRight:'400px'}}>
       <ul>{todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} onToggle={onToggle} />
+        <Todo key={todo.id} todo={todo} dispatch={dispatch} />
       ))}</ul>
 
     </div>
